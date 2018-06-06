2d90eb480e50db9f877aca29796e9bc25a55f411
F. Eugene Aumson <gene@aumson.org>
2018-06-06 18:28:24 -0400

    consolidate duplicate code

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -4,25 +4,26 @@
 
 #include "gdax-orderbook.hpp"
 
-int main(int argc, char* argv[]) {
-    GDAXOrderBook book("ETH-USD");
-
+void printBestBidAndOffer(GDAXOrderBook & book)
+{
     std::cout << "current best bid: Ξ" << book.bids.begin()->second << " @ $"
                                << book.bids.begin()->first << "/Ξ ; ";
     std::cout << "current best offer: Ξ" << book.offers.begin()->second << " @ $"
                                  << book.offers.begin()->first << "/Ξ"
                                  << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+    GDAXOrderBook book("ETH-USD");
+
+    printBestBidAndOffer(book);
 
     size_t secondsToSleep = 5;
     std::cout << "waiting " << secondsToSleep << " seconds for the market to "
         "shift" << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(secondsToSleep));
 
-    std::cout << "current best bid: Ξ" << book.bids.begin()->second << " @ $"
-                               << book.bids.begin()->first << "/Ξ ; ";
-    std::cout << "current best offer: Ξ" << book.offers.begin()->second << " @ $"
-                                 << book.offers.begin()->first << "/Ξ"
-                                 << std::endl;
+    printBestBidAndOffer(book);
 
     size_t histogram[30];
     for ( auto bucket : histogram ) bucket = 0;
